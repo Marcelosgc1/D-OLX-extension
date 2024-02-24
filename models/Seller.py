80912e0b4bf41b286ad21ebbbcb2c7328c0d5a2a
marselosgcc <89476116+Marcelosgc1@users.noreply.github.com>
2024-02-24 10:07:28 -0300

    novos atributos de usuários

--- models/Seller.py
+++ models/Seller.py
@@ -1,7 +1,7 @@
 from models.User import User
 class Seller(User):
-    def __init__(self, name:str, age:int, id:int):
-        super().__init__(name, age, id)
+    def __init__(self, name:str, age:int, id:int, phone, email, user, password):
+        super().__init__(name, age, id, phone, email, user, password)
         self.produtos=[]
 
     def buscarItem():
--- models/User.py
+++ models/User.py
@@ -1,6 +1,9 @@
 class User:
-    def __init__(self, name:str, age:int, id:int):
+    def __init__(self, name:str, age:int, id:int, phone, email, user, password):
         self.name=name
         self.age=age
         self.id=id
-        
+        self.phone=phone
+        self.email=email
+        self.user=user
+        self.password=password
