8d77a2be5302b10e172f1bd1a8a67f84205ee0ba
marselosgcc <89476116+Marcelosgc1@users.noreply.github.com>
2024-03-02 12:19:52 -0300

    removendo testes

--- models/UsersFactory.py
+++ models/UsersFactory.py
@@ -6,7 +6,6 @@ class User_Factory:
     @staticmethod
     def criar_Usuario(name, age, id, phone, email, password, tipo):
         if id in Persistency.get_instance().get_usuarios():
-            print('sextouuuu')
             return False
         elif tipo=='Buyer':
             return Buyer(name, age, id, phone, email, password)
